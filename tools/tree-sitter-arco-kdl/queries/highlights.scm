d982574ea5ccc9e77e997c3c52e9c78041dbdc05
pesap <pesap@users.noreply.github.com>
2026-04-12 18:20:21 +0000

    style(treesitter): adding highlighting (#135)
    * style(treesitter): adding highlighting
    
    * chore: adding instructions and better highligh

--- tools/tree-sitter-arco-kdl/queries/highlights.scm
+++ tools/tree-sitter-arco-kdl/queries/highlights.scm
@@ -1,15 +1,83 @@
 ; Arco KDL Tree-sitter Highlight Queries
-; Extends base KDL with Arco-specific node highlighting
+; Keep structural elements distinct:
+; - Predicates/keywords (`data`, `param`, `set`, ...)
+; - Node names (positional values)
+; - Properties (`from=generator`, etc.)
 
 ; Types
 (type) @type
 (annotation_type) @type.builtin
 
-; Properties
-(prop (identifier) @property)
+; Node predicate / declaration keywords (default KDL nodes)
+(kdl_node (identifier) @function)
 
-; Variables
-(identifier) @variable
+((kdl_node
+  (identifier) @keyword)
+ (#any-of? @keyword
+  "set"
+  "data"
+  "param"
+  "model"
+  "scenario"
+  "map"
+  "index"
+  "in"
+  "filter"
+  "control"
+  "expression"
+  "constraint"
+  "minimize"
+  "maximize"
+  "if"
+  "lower"
+  "upper"
+  "bounds"
+  "slack"
+  "report"
+  "use"
+  "use_data"
+  "reduce"))
+
+; Arco-specific literal-name nodes
+[
+  "constraint"
+  "expression"
+  "expr"
+  "minimize"
+  "maximize"
+  "filter"
+  "if"
+  "lower"
+  "upper"
+] @keyword
+
+(arco_pure_math_node
+  name: (
+    "expression"
+    "minimize"
+    "maximize"
+    "expr"
+    "filter"
+    "if"
+    "lower"
+    "upper"
+  ) @keyword)
+
+(arco_constraint_node
+  name: "constraint" @keyword)
+
+; Node names (e.g. `set thermal`, `model dispatch_model`)
+(node_field
+  (value
+    [
+      (bare_identifier)
+      (string)
+    ] @variable.parameter))
+
+; Properties (`from=...`, `index=...`, etc.)
+(prop (identifier) @property)
+(prop (value (bare_identifier) @property))
+(prop (value (string) @property))
 
 ; Operators
 [
@@ -41,21 +109,6 @@
 (single_line_comment) @comment
 (multi_line_comment) @comment
 
-; Arco-specific nodes (arco_kdl extension over base KDL)
-(arco_pure_math_node
-  name: (
-    "expression"
-    "minimize"
-    "maximize"
-    "expr"
-    "filter"
-    "if"
-    "lower"
-    "upper"
-  ) @keyword.function)
-
-(arco_constraint_node
-  name: "constraint" @keyword.function)
-
+; Opaque algebra payloads (for injected math grammar)
 (arco_math_text) @string.special
 (arco_constraint_math_text) @string.special
